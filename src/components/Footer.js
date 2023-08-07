f62cfd9b3a2719cea73440b6b75310ddabfb2449
Masami <msm-k0304@hotmail.com>
2023-08-07 22:18:46 +1200

    add scroll to top

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,14 @@
 import { Link } from 'react-router-dom'
+import { useEffect } from 'react'
 
 // styles&images
 import './scss/Footer.scss'
 import github from '../assets/github.png'
 import linkedin from '../assets/linkedin.png'
+import upicon from '../assets/arrow_circle_up.svg'
 
 export default function Footer() {
+
   return (
     <footer className='footer'>
       <div>
@@ -22,18 +25,36 @@ export default function Footer() {
       </div>
       
       <div className='footer-copyright'>
-        <a href='https://github.com/Masamist?tab=repositories'>
-          <img src={github} alt='GitHub' />
-        </a>
-        <a href='https://www.linkedin.com/in/masami-stonebridge-0a0a35253/'>
-          <img src={linkedin} alt='LinkedIn' />
-        </a> 
-        <p>
-          2023 Masami Stonebridge Portfolio, All Rights Reserved
-        </p>
+        <TopButton />
+        <div>
+          <a href='https://github.com/Masamist?tab=repositories'>
+            <img src={github} alt='GitHub' />
+          </a>
+          <a href='https://www.linkedin.com/in/masami-stonebridge-0a0a35253/'>
+            <img src={linkedin} alt='LinkedIn' />
+          </a> 
+          <p>
+            2023 Masami Stonebridge Portfolio, All Rights Reserved
+          </p>
+        </div>
+        
       </div>
       
     </footer>
     
+  )
+}
+
+const TopButton = () => {
+
+  useEffect(() => {
+    window.scrollTo({top: 0, left:0, behavior: 'smooth'})
+  }, [])
+  return (
+    <button onClick={()=> {
+      window.scrollTo({top: 0, left:0, behavior:'smooth'})
+    }} className='btn-scroll'>
+      <img src={upicon} alt='Go Top' />
+    </button>
   )
 }
